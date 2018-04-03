33e79b5a432e7baf5d0b2693e92d8e6e646d7fe1
Arthur 'Woosy <dufourarthur.perso@gmail.com>
2018-04-03 08:29:20 +0200

    Changement tokens de connexion

--- Login.py
+++ Login.py
@@ -1,20 +1,22 @@
 import tweepy
 
+
 # Fonction contenant les tokens de connexion au compte :
 def getCfg():
   cfg = {
-    "consumer_key"        : "1HB62XbTj8rsQTOe8ekVxcPKe",
-    "consumer_secret"     : "y3JWxmEe4qYtWByqy6o7MR1pn8VjCUkwDgDJe2dhHJ99ZMOKPy",
+    "consumer_key"        : "Xs6tau7XvQJ7JqnITb8lhPv4d",
+    "consumer_secret"     : "0g993vT7LB07lGAAZdwWgn2IrRzHrlcw9ifdOJ2sLXxQdU9VBL",
     "access_token"        : "965887393923510273-J2uXwagDFx4w3GzYBHjS4LmNumqCcTn",
     "access_token_secret" : "HJ5NA2FavHNf2wZWQis25mJZUwhFPTddhL5JQw7GkUeQr"
   }
   return cfg
 
+
 # Tokens de connexion (pour le stream) :
-consumer_key = "1HB62XbTj8rsQTOe8ekVxcPKe"
-consumer_secret = "y3JWxmEe4qYtWByqy6o7MR1pn8VjCUkwDgDJe2dhHJ99ZMOKPy"
-access_token = "965887393923510273-J2uXwagDFx4w3GzYBHjS4LmNumqCcTn"
-access_token_secret = "HJ5NA2FavHNf2wZWQis25mJZUwhFPTddhL5JQw7GkUeQr"
+consumer_key = "Xs6tau7XvQJ7JqnITb8lhPv4d"
+consumer_secret = "0g993vT7LB07lGAAZdwWgn2IrRzHrlcw9ifdOJ2sLXxQdU9VBL"
+access_token = "965887393923510273-NiATaYXEduiWcdXFeZpnsqa4oY2k36v"
+access_token_secret = "PrnY47GpSoDC9BVuGsoYdDmgqrZLwk15avHilBevIQIyJ"
 
 
 
